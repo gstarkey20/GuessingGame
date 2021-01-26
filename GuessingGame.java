5604213cef0dc5f45f6e4bf78fba5c2a78504afd
gstarkey20 <gstarkey1@catamount.wcu.edu>
2021-01-26 12:05:52 -0500

    added game file

---
+++ GuessingGame.java
@@ -0,0 +1,6 @@
+/*
+ * @author Garrett Starkey
+ *
+ * Java program that allows the user to guess a randomly generated number by the computer. Different messages will be displayed on the console 
+ * depending on if the guessed number is higher or lower than the generated number.
+*/
